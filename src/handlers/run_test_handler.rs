bfc33258956b523de46a53c7fb4c462a988065fe
Wrongways <jez@wain.name>
2023-05-29 13:51:35 +0000

    Launch rapl monitor and firestarter

--- src/handlers/run_test_handler.rs
+++ src/handlers/run_test_handler.rs
@@ -1,6 +1,10 @@
-use axum::{response::IntoResponse, Json};
+use axum::Json;
 use serde::Serialize;
 use crate::model::FirestarterParams;
+use crate::firestarter::Firestarter;
+use crate::rapl::monitor_rapl::monitor_rapl;
+use std::sync::mpsc;
+use std::thread;
 
 
 #[derive(Debug, Serialize)]
@@ -13,11 +17,19 @@ pub async fn run_test_handler(Json(body): Json<FirestarterParams>) -> Json<Rc> {
 
     println!("{body:?}");
     // start rapl monitor
-
+    let (rapl_tx, rapl_rx) = mpsc::channel();
+    let rapl_thread = thread::spawn(move || monitor_rapl(&rapl_rx));
 
     // start firestarter
+    let firestarter = Firestarter::new(body);
+    firestarter.run();
 
+    rapl_tx.send(())
+        .expect("Failed to send halt message to rapl monitor");
 
+    let rapl_stats = rapl_thread.join()
+        .expect("Failed to join rapl thread and receive data");
 
+    println!("RAPL stats: {rapl_stats:?}");
     Json(Rc{wally: String::from("was here")})
 }
