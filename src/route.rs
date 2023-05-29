827d0546222a8e1ccd50de3460c164a0dc281d00
Wrongways <jez@wain.name>
2023-05-29 09:28:14 +0000

    Add test handler

--- src/route.rs
+++ src/route.rs
@@ -1,8 +1,9 @@
 use axum::{routing::get, Router};
-use crate::handlers::system_info_handler;
+use crate::handlers::{system_info_handler, run_test_handler};
 
 
 pub fn create_router() -> Router {
     Router::new()
         .route("/api/system_info", get(system_info_handler))
+        .route("/api/run_test", post(run_test_handler))
 }
