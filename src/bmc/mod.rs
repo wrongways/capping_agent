273fbae2a995b272e2e3ddf5f26be0c0a5343459
Wrongways <jez@wain.name>
2023-06-01 17:10:57 +0000

    Extract BMCStats struct

--- src/bmc/mod.rs
+++ src/bmc/mod.rs
@@ -1,2 +1,25 @@
 pub mod bmc;
 pub mod monitor_bmc;
+use crate::bmc::bmc::BMC_CapSetting;
+
+use chrono::{DateTime, Utc};
+
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+pub struct BMCStats {
+    pub timestamp: DateTime<Utc>,
+    pub power: u64,
+    pub cap_level: u64,
+    pub cap_is_active: bool,
+}
+
+impl BMCStats {
+    pub fn new(power: u64, cap_settings: &BMC_CapSetting) -> Self {
+        Self {
+            timestamp: Utc::now(),
+            power,
+            cap_level: cap_settings.power_limit,
+            cap_is_active: cap_settings.is_active,
+        }
+    }
+}
