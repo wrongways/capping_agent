94066980c6a3329410f9a681d9bac60472bf84c0
Wrongways <jez@wain.name>
2023-06-01 17:12:22 +0000

    Refactor with global lazy CONFIGURATION

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,13 +1,13 @@
 use simple_logger::SimpleLogger;
 use log::info;
-use std::sync::mpsc;
+use std::sync::mpsc::{self, Receiver};
 use std::thread;
-use clap::Parser;
 
 use agent::model::{FirestarterParams, RaplRecord, ServerInfo};
 use agent::bmc::monitor_bmc::monitor_bmc;
-use agent::CLI;
-use agent::test::{load_iterator::LoadTestSuite, thread_iterator::ThreadTestSuite};
+use agent::bmc::BMCStats;
+use agent::test::{load_iterator::LoadTestSuite, thread_iterator::ThreadTestSuite, Test};
+use agent::CONFIGURATION;
 
 
 const AGENT_INFO_ENDPOINT: &str = "http://oahu10000:8000/api/system_info";
@@ -17,8 +17,6 @@ const AGENT_RUN_TEST_ENDPOINT: &str = "http://oahu10000:8000/api/run_test";
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     SimpleLogger::new().env().init()?;
 
-    let args = CLI::parse();
-
     let host_info: ServerInfo = reqwest::get(AGENT_INFO_ENDPOINT)
         .await?
         .json()
@@ -26,34 +24,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("Host info:\n{host_info:?}");
 
-    let load_iterator = LoadTestSuite::new();
-    let thread_iterator = ThreadTestSuite::new(host_info.system_info.online_cpus);
 
-    for test in load_iterator.iter {
-        println!("{test:?}")
+    let load_tests = LoadTestSuite::new();
+    let thread_tests = ThreadTestSuite::new(host_info.system_info.online_cpus);
+
+    let total_runtime_secs = CONFIGURATION.warmup_secs + CONFIGURATION.test_time_secs;
+
+    for test in load_tests {
+        println!("{test:?}");
     }
 
-    for test in thread_iterator.iter {
-        println!("{test:?}")
+    for test in thread_tests {
+        println!("{test:?}");
+        let (rapl_stats, bmc_stats) = run_test(test, total_runtime_secs).await?;
+        info!("RAPL stats\n{rapl_stats:?}");
+        info!("\n\nBMC Stats\n{bmc_stats:?}");
     }
 
-    // Start BMC monitor
+    Ok(())
+}
+
+async fn run_test(config: Test, runtime_secs: u64) ->
+    Result<(Vec<RaplRecord>, Vec<BMCStats>), Box<dyn std::error::Error>> {
+
     let (bmc_tx, bmc_rx) = mpsc::channel();
-    let bmc_thread = thread::spawn(move ||
-        monitor_bmc(
-            &args.bmc_hostname,
-            &args.bmc_username,
-            &args.bmc_password,
-            &args.ipmi,
-            bmc_rx
-        )
-    );
+    let bmc_thread = start_bmc_monitor(bmc_rx);
 
     let fs_params = FirestarterParams {
-        runtime_secs: 20,
-        load_pct: 100,
-        load_period_us: 0,
-        n_threads: 0,
+        runtime_secs,
+        load_pct: config.load_pct,
+        load_period_us: config.load_period,
+        n_threads: config.n_threads
     };
 
     let rapl_stats: Vec<RaplRecord> = reqwest::Client::new()
@@ -65,12 +66,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     bmc_tx.send(()).expect("Failed to signal BMC thread");
-    let bmc_stats = bmc_thread.join().expect("Failed to join BMC thread");
+    let bmc_stats: Vec<BMCStats> = bmc_thread.join().expect("Failed to join BMC thread");
 
-    info!("RAPL stats\n{rapl_stats:?}");
-    info!("\n\nBMC Stats\n{bmc_stats:?}");
-
-    Ok(())
+    Ok((rapl_stats, bmc_stats))
 }
 
+fn start_bmc_monitor(rx_channel: Receiver<()>) -> thread::JoinHandle<Vec<BMCStats>> {
+    thread::spawn(move ||
+        monitor_bmc(rx_channel)
+    )
+}
 
--- src/bmc/monitor_bmc.rs
+++ src/bmc/monitor_bmc.rs
@@ -1,60 +1,29 @@
-use crate::bmc::bmc::{BMC, BMC_CapSetting};
+use crate::bmc::bmc::BMC;
+use crate::bmc::BMCStats;
 use log::{info, trace};
-use std::fmt;
 use std::sync::mpsc::Receiver;
 use std::thread;
 use std::time::Duration;
-use chrono::{DateTime, Utc, SecondsFormat};
+use crate::CONFIGURATION;
 
 const BMC_INTER_COMMAND_SLEEP_MILLIS: u64 = 500;
 const BMC_POLL_INTERVAL_MILLIS: u64 = 500;
 
-#[allow(non_camel_case_types)]
-#[derive(Debug)]
-pub struct BMC_Stats {
-    pub timestamp: DateTime<Utc>,
-    pub power: u64,
-    pub cap_level: u64,
-    pub cap_is_active: bool,
-}
-
-impl fmt::Display for BMC_Stats {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{},{},{},{}",
-            self.timestamp.to_rfc3339_opts(SecondsFormat::Millis, false),
-            self.power,
-            self.cap_level,
-            self.cap_is_active,
-        )
-    }
-}
-
-impl BMC_Stats {
-    pub fn new(power: u64, cap_settings: &BMC_CapSetting) -> Self {
-        Self {
-            timestamp: Utc::now(),
-            power,
-            cap_level: cap_settings.power_limit,
-            cap_is_active: cap_settings.is_active,
-        }
-    }
-}
 
 /// Periodically polls the BMC for power reading and saves the result. Runs on its own thread.
 /// Each time through the loop, checks for a message from the main monitor thread that signals
 /// that this thread can exit. Before exiting, saves results to CSV file.
-pub fn monitor_bmc(
-    bmc_hostname: &str,
-    bmc_username: &str,
-    bmc_password: &str,
-    ipmi: &str,
-    rx: Receiver<()>) -> Vec::<BMC_Stats> {
+pub fn monitor_bmc(rx: Receiver<()>) -> Vec::<BMCStats> {
     info!("\tBMC: launched");
 
-    let mut stats = Vec::<BMC_Stats>::new();
-    let bmc = BMC::new(&bmc_hostname, &bmc_username, &bmc_password, &ipmi);
+    let mut stats = Vec::<BMCStats>::new();
+    let bmc = BMC::new(
+        &CONFIGURATION.bmc_hostname,
+        &CONFIGURATION.bmc_username,
+        &CONFIGURATION.bmc_password,
+        &CONFIGURATION.ipmi
+    );
+
     loop {
         // Check if monitor master asked us to exit with a message on the channel
         if rx.try_recv().is_ok() {
@@ -66,7 +35,7 @@ pub fn monitor_bmc(
         let current_power = bmc.current_power();
         thread::sleep(Duration::from_millis(BMC_INTER_COMMAND_SLEEP_MILLIS));
         let current_cap_settings = bmc.current_cap_settings();
-        let reading = BMC_Stats::new(current_power, &current_cap_settings);
+        let reading = BMCStats::new(current_power, &current_cap_settings);
 
         trace!("BMC power reading: {reading:#?}");
         stats.push(reading);
